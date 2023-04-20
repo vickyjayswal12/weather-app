bba961a6388119a1c45a63ad5ee18d26d4dadf06
Vicky <vickyjayswal12@gmail.com>
2023-04-20 10:09:45 +0530

    kalvin to cenlius convert

--- backend/index.js
+++ backend/index.js
@@ -26,8 +26,10 @@ app.post('/temp',(req,resp)=>
     let url="https://api.openweathermap.org/data/2.5/weather?q="+city+"&appid=d2239184eefb923448a5a7204d7f8397";
     requests(url).on("data",(chunk)=>{
         const obj=JSON.parse(chunk);
-        // console.log(obj.main.temp);
-         resp.render('home1',{temp:obj,city_name:city});
+        console.log(obj.main.temp);
+        let f=obj.main.temp-273.15;
+
+         resp.render('home1',{temp:f.toFixed(2),city_name:city});
     })
     // .on("end",(err)=>{
     //     if(err)return console.log("connection closed due to error",err);
